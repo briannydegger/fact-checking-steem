a61ec29e5cc957018bafb28f56c38c27c35d1e5a
Brian Nydegger <briannydegger@gmail.com>
2018-11-30 16:17:10 +0100

    Update about page with more details

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,17 @@
 <template>
-  <div>
-    <p>About</p>
-  </div>
+    <div>
+        <p>Fact-cheking Steem is a website using the wisdom of the crowd and the Steem blockchain to try to detect fake facts.</p>
+        <p>Users can post any fact / article and let others comment and vote to say if they think it's true or false. The blockchain steem rewards users bringing relevant comments and those who vote in the direction of the popular opinion.</p>
+        <p>
+            This project was made by
+            <a href="https://steemit.com/@irtiss">Brian</a> during is thesis of Master of Science HES-SO in Engineering.
+        </p>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "about",
-  components: {}
+    name: "about",
+    components: {}
 };
 </script>
