dc0aec8f6b8d1e8a9a52c93451e04dfb321f9a81
Brian Nydegger <briannydegger@gmail.com>
2018-12-06 16:14:20 +0100

    Fix warning: remove arguments on fonction scrollBehavior

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Router from "vue-router";
 Vue.use(Router);
 
 export default new Router({
-    scrollBehavior(to, from, savedPosition) {
+    scrollBehavior() {
         return {
             x: 0,
             y: 0
