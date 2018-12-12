d71d2b9110fea7af2e810609f0807659fa96bf6d
Brian Nydegger <briannydegger@gmail.com>
2018-12-12 11:32:07 +0100

    Update underline for majority

--- src/components/VoteLine.vue
+++ src/components/VoteLine.vue
@@ -2,13 +2,13 @@
     <span>
         {{ votes.positiveVote + votes.negativeVote + votes.nullVote }} votes (&#128077; :
         <span
-            v-bind:class="{ 'vote-highlight': votes.positiveVote >= votes.negativeVote && votes.positiveVote >= votes.nullVote }"
+            v-bind:class="{ 'vote-highlight': votes.positiveVote >= votes.negativeVote && votes.positiveVote > votes.nullVote }"
         >{{ votes.positiveVote }}</span> | &#128528; :
         <span
-            v-bind:class="{ 'vote-highlight': votes.nullVote > votes.negativeVote && votes.nullVote > votes.positiveVote }"
+            v-bind:class="{ 'vote-highlight': votes.nullVote >= votes.negativeVote && votes.nullVote >= votes.positiveVote }"
         >{{ votes.nullVote }}</span> | &#128078; :
         <span
-            v-bind:class="{ 'vote-highlight': votes.negativeVote > votes.positiveVote && votes.negativeVote >= votes.nullVote }"
+            v-bind:class="{ 'vote-highlight': votes.negativeVote > votes.positiveVote && votes.negativeVote > votes.nullVote }"
         >{{ votes.negativeVote }}</span>)
     </span>
 </template>
