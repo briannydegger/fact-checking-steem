8de0b161719dba2870c2c751dad2a6c0ed52cb32
Brian Nydegger <briannydegger@gmail.com>
2019-01-14 11:29:04 +0100

    Fix images and a link on about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,7 +4,7 @@
         <p>Users can post any fact / article and let others comment and vote to say if they think it's true or false. The blockchain steem rewards users bringing relevant comments and those who vote in the direction of the popular opinion.</p>
         <p>
             This project was made by
-            <a href="https://steemit.com/@irtiss">Brian</a> during is thesis of Master of Science HES-SO in Engineering.
+            <a href="https://steemit.com/@irtiss" target="_blank">Brian</a> during is thesis of Master of Science HES-SO in Engineering.
         </p>
         <p>
             Read more about :
